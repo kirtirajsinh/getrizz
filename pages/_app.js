99a074a78dab73b69c241fda1e67f506d8795991
kirtirajsinh <kirtiraj3738@gmail.com>
2023-04-13 22:38:56 +0530

    create protected route

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import Nav from "@/components/Nav";
 import "@/styles/globals.css";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function App({
   Component,
@@ -8,8 +10,31 @@ export default function App({
 }) {
   return (
     <SessionProvider session={session}>
+      <RequireAuth />
       <Nav />
       <Component {...pageProps} />;
+      <RequireAuth />
     </SessionProvider>
   );
+}
+
+function RequireAuth({ children }) {
+  const { data: sessionData, status } = useSession();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Redirect to login page if session is not loaded yet
+  useEffect(() => {
+    if (status !== "loading") setIsLoading(false);
+  }, [status]);
+
+  if (isLoading) return null;
+
+  // Redirect to login page if user is not authenticated and is trying to access a protected page
+  if (!sessionData?.user && router.pathname !== "/") {
+    router.push("/");
+    return null;
+  }
+
+  return children;
 }

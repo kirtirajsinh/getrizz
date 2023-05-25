7bad7cea0ac881da35a43f59c33c6372aa32f38a
kirtirajsinh <kirtiraj3738@gmail.com>
2023-05-25 22:29:02 +0530

    fix auth 🤞

--- server/auth.js
+++ server/auth.js
@@ -12,6 +12,40 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async session({ token, session }) {
+      console.log("session", session);
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+
+      return session;
+    },
+    async jwt({ token, user }) {
+      const dbUser = await prisma.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      });
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id;
+        }
+        return token;
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
+    },
+  },
 };
 
 export const getServerAuthSession = (ctx) => {
